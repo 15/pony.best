d99dd2ca806a0320cc933dc1b3a179ae62dc0313
15 <146901496+15@users.noreply.github.com>
2024-08-26 12:14:49 -0700

    Blog

---
+++ src/data/blog.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        id: 0,
+        title: "Twilight Sparkle",
+        description: "Twilight Sparkle is the best pony!",
+        date: "10 August 2024",
+        content: `
+        <div>
+
+        </div>
+        `
+    },
+    {
+        id: 1,
+        title: "Pony conventions",
+        description: "Pony cons I have attended and are planning to attend",
+        date: "22 August 2024",
+        content: `
+        <div>
+        The following is a list of pony conventions I've attended so far:
+        <ul class="list-inside list-['-_'] text-base pl-4">
+            <li>Mare Fair 2023</li>
+            <li>HarmonyCon 2024</li>
+            <li>BABSCon 2024</li>
+            <li>Whinny City 2024</li>
+            <li>TrotCon 2024</li>
+            <li>Everfree Northwest 2024 (currently attending)</li>
+        </ul>
+        </div>
+        <div class="flex flex-col gap-y-1 justify-center items-center my-4">
+        <img src="/marefairpostcard.png" alt="Mare Fair postcard" width="200" class="w-64 h-auto"/>
+        <span class="text-xs"><span class="font-semibold">Fig 1.</span> I will be at Mare Fair 2024!</span>
+        </div>
+        <div>
+        I'm also planning to attend the following upcoming pony cons:
+        <ul class="list-inside list-['-_'] text-base pl-4">
+            <li>Mare Fair 2024</li>
+            <li>Ponyville Ciderfest 2024</li>
+            <li>HarmonyCon 2025</li>
+            <li>BABSCon 2025</li>
+        </ul>
+        If you spot me at one of these cons, feel free to say hi. I will be wearing a lanyard with the name "15" on it.
+        </div>
+        `
+    },
+];
