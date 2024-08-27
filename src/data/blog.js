70e40f6d26f10c6640d2903f600e74d9feee18af
15 <146901496+15@users.noreply.github.com>
2024-08-27 19:00:25 -0400

    blog post

--- src/data/blog.js
+++ src/data/blog.js
@@ -3,10 +3,14 @@ export default [
         id: 0,
         title: "Twilight Sparkle",
         description: "Twilight Sparkle is the best pony!",
-        date: "10 August 2024",
+        date: "20 August 2024",
         content: `
+        <div class="my-4 flex flex-col gap-y-1 justify-center items-center">
+        <img src="/twi.png" alt="Mare Fair postcard" width="200" class="border-2 border-black rounded bg-white w-64 h-auto"/>
+        <span class="text-xs"><span class="font-bold">Fig. 1:</span> Twilight Sparkle</span>
+        </div>
         <div>
-        Twilight Sparkle is the best pony!
+        Twilight Sparkle is the best pony! Just look at her. How can you think she isn't the cutest, prettiest, and smartest pony in all of Equestria?
         </div>
         `
     },
@@ -14,32 +18,77 @@ export default [
         id: 1,
         title: "Pony conventions",
         description: "Pony cons I have attended and are planning to attend",
-        date: "22 August 2024",
+        date: "24 August 2024",
         content: `
+        <div class="my-4 flex flex-col gap-y-1 justify-center items-center">
+        <img src="/marefairpostcard.png" alt="Mare Fair postcard" width="200" class="border-2 border-black rounded bg-white w-64 h-auto"/>
+        <span class="text-xs"><span class="font-bold">Fig. 1:</span> Mare Fair 2024</span>
+        </div>
         <div>
         The following is a list of pony conventions I've attended so far:
-        <ul class="list-inside list-['-_'] text-base pl-4">
+        <ul class="list-inside list-['-_'] pl-4 my-1">
             <li>Mare Fair 2023</li>
             <li>HarmonyCon 2024</li>
             <li>BABSCon 2024</li>
             <li>Whinny City 2024</li>
             <li>TrotCon 2024</li>
-            <li>Everfree Northwest 2024 (currently attending)</li>
+            <li>Everfree Northwest 2024</li>
         </ul>
-        </div>
-        <div class="flex flex-col gap-y-1 justify-center items-center my-4">
-        <img src="/marefairpostcard.png" alt="Mare Fair postcard" width="200" class="w-64 h-auto"/>
-        <span class="text-xs"><span class="font-semibold">Fig 1.</span> I will be at Mare Fair 2024!</span>
         </div>
         <div>
         I'm also planning to attend the following upcoming pony cons:
-        <ul class="list-inside list-['-_'] text-base pl-4">
+        <ul class="list-inside list-['-_'] pl-4 my-1">
             <li>Mare Fair 2024</li>
             <li>Ponyville Ciderfest 2024</li>
             <li>HarmonyCon 2025</li>
             <li>BABSCon 2025</li>
         </ul>
-        If you spot me at one of these cons, feel free to say hi!
+        If you spot me at one of these cons, feel free to say hi! Mention this website and I might give you a special prize.
+        </div>
+        `
+    },
+    {
+        id: 2,
+        title: "Everfree Northwest 2024",
+        description: "A retrospective of EFNW '24",
+        date: "27 August 2024",
+        content: `
+        <div class="my-4 flex flex-col gap-y-1 justify-center items-center">
+        <img src="/marefairpostcard.png" alt="Mare Fair postcard" width="200" class="border-2 border-black rounded bg-white w-64 h-auto"/>
+        <span class="text-xs"><span class="font-bold">Fig. 1:</span> Mare Fair 2024</span>
+        </div>
+        <div>
+        Everfree Northwest '24 was a pony convention from August 23 to 25, 2024. 
+        </div>
+        `
+    },
+    {
+        id: 3,
+        title: "/mlp/ TF2",
+        description: "A retrospective of /mlp/ TF2",
+        date: "27 August 2024",
+        content: `
+        <div class="my-4 flex flex-col gap-y-1 justify-center items-center">
+        <img src="/marefairpostcard.png" alt="Mare Fair postcard" width="200" class="border-2 border-black rounded bg-white w-64 h-auto"/>
+        <span class="text-xs"><span class="font-bold">Fig. 1:</span> The /mlp/ TF2 server</span>
+        </div>
+        <div>
+        There's no doubt that the pony fandom has a storied histor with the popular first-person shooter video game <span class="italic">Team Fortress 2</span>.
+        </div>
+        `
+    },
+    {
+        id: 4,
+        title: "<span class='italic'>My Little Pony: Friendship Is Magic</span>",
+        description: "A retrospective of the show",
+        date: "27 August 2024",
+        content: `
+        <div class="my-4 flex flex-col gap-y-1 justify-center items-center">
+        <img src="/marefairpostcard.png" alt="Mare Fair postcard" width="200" class="border-2 border-black rounded bg-white w-64 h-auto"/>
+        <span class="text-xs"><span class="font-bold">Fig. 1:</span> The /mlp/ TF2 server</span>
+        </div>
+        <div>
+        There's no doubt that the pony fandom has a storied histor with the popular first-person shooter video game <span class="italic">Team Fortress 2</span>.
         </div>
         `
     },
